a6051b5baa610f9910f4fac90648bf12012057f3
Ilya Tikhonov <mr.ilia.tikhonov@gmail.com>
2024-10-30 23:19:33 +0200

    4 practice

--- 6.php
+++ 6.php
@@ -5,11 +5,12 @@
 // php 6.php list yesterday
 // php 6.php add "Wake up"
 // php 6.php add "Drink coffee"
-// php 6.php complete 1 2
+// php 6.php done 1 2 [x]
+// php 6.php undone 1 2 [ ]
 // php 6.php remove 2 (rm)
 
 
-function main (array $arguments): void
+function main (array $arguments)
 {
 //	unset($arguments[0]); //выбрасывают 1 аргумент
 	array_shift($arguments);
@@ -22,13 +23,17 @@ function main (array $arguments): void
 	{
 		case "list":
 			listCommand($arguments);
+			break;
 		case "add":
 			addCommand($arguments);
 			break;
-		case "complete":
-			 completeCommand($arguments);
+		case "done":
+			doneCommand($arguments);
 			break;
-		case "remove":
+		case "undone":
+			undoneCommand($arguments);
+			break;
+		case "rm":
 			removeCommand($arguments);
 			break;
 		default:
@@ -40,8 +45,6 @@ function main (array $arguments): void
 	exit (0); //Команды успешно выполнились
 }
 
-
-
 function addCommand(array $arguments)
 {
 	$title = array_shift($arguments); //Заголовок - 1-й элемент
@@ -50,6 +53,9 @@ function addCommand(array $arguments)
 		'id' => uniqid(),
 		'title' => $title,
 		'completed' => false,
+		'created_at' => time(), //текущее время
+		'updated_at ' => null,
+		'completed_at ' => null,
 	];
 
 //	$serializedString = serialize($todo); //строковое представление todo
@@ -61,7 +67,7 @@ function addCommand(array $arguments)
 
 	if (file_exists($filePath)) //Проверка на существование
 	{
-		$content = file_get_contents(($filePath)); //Содержимое файла
+		$content = file_get_contents($filePath); //Содержимое файла
 		$todos = unserialize($content, [ //Преобразуем в массив, небезопасно
 			'allowed_classes' => false,  //Для безопасности
 		]);
@@ -82,13 +88,126 @@ function addCommand(array $arguments)
 
 function removeCommand(array $arguments)
 {
+	$fileName = date('Y-m-d') . 'txt';
+	$filePath = __DIR__ . '/data/' . $fileName; //Текущая директория + папка data + fileName. Абсолютный путь к файлу
 
+	if (!file_exists($filePath))
+	{
+		echo "Nothing to do here";
+		return;
+	}
 
+	$content = file_get_contents($filePath); //Содержимое файла
+	$todos = unserialize($content, [ //Преобразуем в массив, небезопасно
+		'allowed_classes' => false,  //Для безопасности
+	]);
+
+	if (empty($todos)) //Если массив пустой
+	{
+		echo "Nothing to do here";
+		return;
+	}
+
+	foreach ($arguments as $num) //Каждый аргумент как номер
+	{
+		$index = (int)$num - 1;
+
+		if (!isset($todos[$index])) // проверка, что по индексу что-то есть
+		{
+			echo "Task $num does not exist.\n";
+			continue;
+		}
+
+		unset($todos[$index]);
+	}
+
+	$todos = array_values($todos);
+
+	file_put_contents($filePath, serialize($todos));
 }
 
-function completeCommand(array $arguments)
+function doneCommand(array $arguments) //php 6.php done 2
 {
+	$fileName = date('Y-m-d') . 'txt';
+	$filePath = __DIR__ . '/data/' . $fileName; //Текущая директория + папка data + fileName. Абсолютный путь к файлу
 
+	if (!file_exists($filePath))
+	{
+		echo "Nothing to do here";
+		return;
+	}
+
+	$content = file_get_contents(($filePath)); //Содержимое файла
+	$todos = unserialize($content, [ //Преобразуем в массив, небезопасно
+		'allowed_classes' => false,  //Для безопасности
+	]);
+
+	if (empty($todos)) //Если массив пустой
+	{
+		echo "Nothing to do here";
+		return;
+	}
+
+	$now = time();
+
+	foreach ($arguments as $num) //Каждый аргумент как номер
+	{
+		$index = (int)$num - 1;
+		if (!isset($todos[$index])) // проверка, что по индексу что-то есть
+		{
+			echo "Task $num does not exist.\n";
+			continue;
+		}
+		$todos[$index] = array_merge($todos[$index], [ //Слияние 2-х массивов
+			'completed' => true,
+			'updated_at' => $now,
+			'completed_at' => $now,
+		]);
+	}
+
+	file_put_contents($filePath, serialize($todos));
+}
+
+function undoneCommand(array $arguments)
+{
+	$fileName = date('Y-m-d') . 'txt';
+	$filePath = __DIR__ . '/data/' . $fileName; //Текущая директория + папка data + fileName. Абсолютный путь к файлу
+
+	if (!file_exists($filePath))
+	{
+		echo "Nothing to do here";
+		return;
+	}
+
+	$content = file_get_contents(($filePath)); //Содержимое файла
+	$todos = unserialize($content, [ //Преобразуем в массив, небезопасно
+		'allowed_classes' => false,  //Для безопасности
+	]);
+
+	if (empty($todos)) //Если массив пустой
+	{
+		echo "Nothing to do here";
+		return;
+	}
+
+	foreach ($arguments as $num) //Каждый аргумент как номер
+	{
+		$index = (int)$num - 1;
+
+		if (!isset($todos[$index])) // проверка, что по индексу что-то есть
+		{
+			echo "Task $num does not exist.\n";
+			continue;
+		}
+
+		$todos[$index] = array_merge($todos[$index], [ //Слияние 2-х массивов
+			'completed' => false,
+			'updated_at' => time(),
+			'completed_at' => null,
+		]);
+	}
+
+	file_put_contents($filePath, serialize($todos));
 
 }
 
@@ -103,7 +222,7 @@ function listCommand(array $arguments)
 		return;
 	}
 
-	$content = file_get_contents(($filePath)); //Содержимое файла
+	$content = file_get_contents($filePath); //Содержимое файла
 	$todos = unserialize($content, [ //Преобразуем в массив, небезопасно
 		'allowed_classes' => false,  //Для безопасности
 	]);
