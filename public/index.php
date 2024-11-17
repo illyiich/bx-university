dbcbe5b36d03ea9434daa958d10dc638307b4150
Ilya Tikhonov <mr.ilia.tikhonov@gmail.com>
2024-11-17 17:04:05 +0200

    <?= - eto echo

--- public/index.php
+++ public/index.php
@@ -1,3 +1,12 @@
+<?php
+
+require_once  __DIR__ . '/../boot.php';
+
+$title = "Todoist";
+$todos = getTodos();
+
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -6,50 +15,25 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="/style.css">
-    <title>Document</title>
+    <title><?= $title; ?></title>
 </head>
 <body>
     <section class="content">
         <header>
             <span class="icon">📝</span>
-            <h1>Todoist</h1>
+            <h1><?= $title; ?></h1>
         </header>
 
         <main>
 
-            <?php
-
-            require_once  __DIR__ . '/../boot.php';
-
-            $todos = getTodos();
-
-            foreach ($todos as $todo)
-            {
-
-
-
-            ?>
-
-            <article class="todo">
-                <label>
-                    <input type="checkbox" <?php
-                        if ($todo['completed'])
-                        {
-                            echo "checked";
-                        }
-
-                    ?>>
-                    <?php
-                        echo $todo['title']
-
-                    ?>
-                </label>
-            </article>
-
-            <?php
-            }
-            ?>
-
+            <?php foreach ($todos as $todo): ?>
+                <article class="todo">
+                    <label>
+                        <input type="checkbox" <?= ($todo['completed']) ? 'checked' : ''; ?>>
+                        <?= $todo['title']; ?>
+                    </label>
+                </article>
+            <?php endforeach; ?>
 
             <form action="/" method="post" class="add-todo">
                 <input type="text" placeholder="What to do?">
@@ -58,7 +42,7 @@
         </main>
 
         <footer>
-            &copy; 2030 Todoist by Bitrix University
+            &copy; <?= date('Y'); ?> Todoist by Bitrix University
         </footer>
 
     </section>
